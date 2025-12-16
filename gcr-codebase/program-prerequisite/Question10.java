3e033a29fc76fdc36a94d3b8dd06b4c87b5de5e1
Lucky Pal <pallucky936@gmail.com>
2025-12-16 17:07:29 +0530

    [Lucky Pal]: Completed 10th Java Program

---
+++ gcr-codebase/program-prerequisite/Question10.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Question10{
+       public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       float km = sc.nextFloat();
+       double miles = km*0.621371;
+       System.out.println(miles);
+    }
+}
