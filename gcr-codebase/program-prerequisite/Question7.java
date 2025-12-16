bda1f65b84ff4bbf6241333924362cbea7332e4e
Lucky Pal <pallucky936@gmail.com>
2025-12-16 17:05:07 +0530

    [Lucky Pal]: Completed 7th Java Program

---
+++ gcr-codebase/program-prerequisite/Question7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Question7{
+       public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       float len = sc.nextFloat();
+       float wid = sc.nextFloat();
+       double perimeter = (len+wid)+2;
+       System.out.println(perimeter);
+      }
+}  
