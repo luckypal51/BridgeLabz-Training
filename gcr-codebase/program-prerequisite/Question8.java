13e9b20d43e0570ef1203938c5bec18ee570d50c
Lucky Pal <pallucky936@gmail.com>
2025-12-16 17:05:54 +0530

    [Lucky Pal]: Completed 8th Java Program

---
+++ gcr-codebase/program-prerequisite/Question8.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Question8{
+       public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int pow =sc.nextInt();
+       double ans = Math.pow(n,pow);
+       System.out.println(ans);
+      }
+}
