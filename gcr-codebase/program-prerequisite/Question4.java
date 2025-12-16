c3c1396c82579efc7e306ea101a2fcd7830c194c
Lucky Pal <pallucky936@gmail.com>
2025-12-16 16:42:46 +0530

    [Lucky Pal]: Program-prerequisite added

---
+++ gcr-codebase/program-prerequisite/Question4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Question4{
+	public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+        float area = (3.14)*(radius*radius);
+        System.out.println(area);
+}
+}
---
+++ gcr-codebase/program-prerequisite/Question5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Question5{
+         public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+ 	 float radius = sc.nextFloat();
+         float height = sc.nextFloat();
+         double volume = 3.14*radius*radius*height;
+         System.out.println(volume);
+      }
+}
