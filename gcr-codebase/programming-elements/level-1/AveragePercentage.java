58cbf4cf64c984b99bca8beaf0737fa82af0fe88
Lucky Pal <pallucky936@gmail.com>
2025-12-17 14:31:22 +0530

    [Lucky Pal]: added AveragePercentage file

---
+++ gcr-codebase/programming-elements/level-1/AveragePercentage.java
@@ -0,0 +1,9 @@
+public class AveragePercentage{
+       	public static void main(String[] args){
+        float math = 94;
+        float physics =95;
+        float chemistry= 96;
+        double average =(math+physics+chemistry)/3;
+	System.out.println(average);
+}
+}
