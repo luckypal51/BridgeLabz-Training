11dc883a8085890503700440492b7ce5b963e9da
Lucky Pal <pallucky936@gmail.com>
2025-12-17 15:26:17 +0530

    [Lucky Pal]:added square file

---
+++ gcr-codebase/programming-elements/level-2/Square.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Square{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+        float perimeter = sc.nextFloat();
+        float side = perimeter/4;
+        System.out.println("The length of the side is "+side+" whose perimeter is "+perimeter);
+}
+}
