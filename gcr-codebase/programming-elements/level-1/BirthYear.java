82b616f36efc2382283dae2d18c96b659032f012
Lucky Pal <pallucky936@gmail.com>
2025-12-17 14:32:19 +0530

    [Lucky Pal]:added BirthYear file

---
+++ gcr-codebase/programming-elements/level-1/BirthYear.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class BirthYear{
+        public static void main(String[] args){
+	int birthYear =2000;
+        int currentYear = 2024;
+        System.out.println("Harry's age in 2024 is "+(currentYear-birthYear));
+}
+}
