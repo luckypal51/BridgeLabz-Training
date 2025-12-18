6e27dc1378bfae8641b0c686ed477b400da1b897
Lucky Pal <pallucky936@gmail.com>
2025-12-18 19:08:19 +0530

    [Lucky Pal]: added leapYear2 file

---
+++ gcr-codebase/control-flows/Level-3/LeapYear2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LeapYear2{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		
+		if(year>=1582){
+			if((year%400==0)||(year%4==0&&year%100!=0)){
+						System.out.println("leap year");
+					
+				}else{
+					System.out.println("not a leap year");
+				}
+			}else{
+			System.out.println("enter year after 1582");
+		}
+	}
+}
