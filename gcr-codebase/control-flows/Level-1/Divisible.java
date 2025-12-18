b3a08be32d7c57bccdaeb2145250ad7072cdaae8
Lucky Pal <pallucky936@gmail.com>
2025-12-18 14:38:55 +0530

    [Lucky Pal] :added Divisible file

---
+++ gcr-codebase/control-flows/Level-1/Divisible.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Divisible{
+	public static void main(String[] args){
+	
+	Scanner sc = new Scanner(System.in);
+	float num = sc.nextFloat();
+	
+	if(num%5==0){
+	
+	System.out.println("Is the number "+num+" divisible by 5? Yes");
+	
+	}
+	else {
+	
+	System.out.println("Is the number "+num+" divisible by 5? No");
+	
+	}
+	
+	}}
