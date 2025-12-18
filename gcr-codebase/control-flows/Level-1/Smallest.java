b0e2edd384195bb21ddb14606aea056e3529690c
Lucky Pal <pallucky936@gmail.com>
2025-12-18 14:46:43 +0530

    [Lucky Pal]: added the Smallest number file

---
+++ gcr-codebase/control-flows/Level-1/Smallest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Smallest{
+	public static void main(String[]args){
+	
+	Scanner sc = new Scanner(System.in);
+	
+	float a = sc.nextFloat();
+	float b = sc.nextFloat();
+	float c = sc.nextFloat();
+	
+	if(a<b&&a<c){
+	
+	System.out.println("Is the first number the smallest? Yes");
+	
+	}
+	else{
+	
+	System.out.println("Is the first number the smallest? No");
+	
+	}
+	
+	}
+	
+	
+	}
