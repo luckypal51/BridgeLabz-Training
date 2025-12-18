da86c8d0fa7252ea4d02ab801fab60ab2c5f1793
Lucky Pal <pallucky936@gmail.com>
2025-12-18 11:22:31 +0530

    [Lucky Pal] added ConversionInput file

--- gcr-codebase/programming-elements/level-1/ConversionInput.java
+++ gcr-codebase/programming-elements/level-1/ConversionInput.java
@@ -3,7 +3,7 @@ public class ConversionInput{
  	public static void main(String[]args){
 	Scanner input = new Scanner(System.in);
  	int km = input.nextInt();
-	double miles = km*1.6;
+	double miles = km*1.6; // to convert km into miles
         System.out.println("The total miles is "+miles+"mile for the given "+km+"km");
 }
 }
