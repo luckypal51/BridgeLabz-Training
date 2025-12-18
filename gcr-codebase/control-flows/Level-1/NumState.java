95c824571e828a90cdbe3cfca950ad50fc4218a5
Lucky Pal <pallucky936@gmail.com>
2025-12-18 14:43:33 +0530

    [Lucky Pal]; added NumState file

---
+++ gcr-codebase/control-flows/Level-1/NumState.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class NumState{
+	public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+	int num = sc.nextInt();
+	
+	if(num>0){
+	
+	System.out.println("Positive");
+	
+	}
+	else if(num==0){
+	
+	System.out.println("Zero");
+	
+	}
+	else{
+	
+	System.out.println("Negative");
+	
+	}
+	}
+}
