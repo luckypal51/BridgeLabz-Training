b6c5c50c3289d5cbb8f2815a0bb4658fc7bfb54c
Lucky Pal <pallucky936@gmail.com>
2025-12-22 14:35:34 +0530

    [Lucky Pal]: added ArrayIndex file

---
+++ gcr-codebase/Strings/Level-1/ArrayIndex.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ArrayIndex{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] str = new String[n];
+		for(int i=0;i<n;i++){
+			str[i] = sc.next();
+		}
+		try{
+			generate(str,n);
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	public static void generate(String[] str,int n){
+		String s = str[n+1];
+	}
+}
