90bf59c75f5e84ba857ce45b576ac6c98021d8ca
Lucky Pal <pallucky936@gmail.com>
2025-12-23 19:47:15 +0530

    [Lucky Pal]: added Anagram 2 file

---
+++ gcr-codebase/Extra/String/Anagram2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Anagram2{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		
+		System.out.println("The strings are anagram : "+anagram(s1,s2));
+	}
+	public static boolean anagram(String s1, String s2){
+		int [] arr1 = new int[256];
+		int [] arr2 = new int[256];
+		
+		for(char a:s1.toCharArray()){
+			arr1[a]+=1;
+		}
+			for(char a:s2.toCharArray()){
+			arr2[a]+=1;
+		}
+		
+		for(int i =0;i<256;i++){
+			if(arr1[i]!=arr2[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+}
