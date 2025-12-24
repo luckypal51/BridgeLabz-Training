6951f163d41a84cea4e315cc498c62c00a3ec379
Lucky Pal <pallucky936@gmail.com>
2025-12-24 11:43:40 +0530

    [Lucky Pal]: added Arrays on leetcode folder

---
+++ leet-code-codebase/Arrays/Redistribution.java
@@ -0,0 +1,28 @@
+//3074. Apple Redistribution into Boxes
+//You are given an array apple of size n and an array capacity of size m.
+
+//There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+//Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+
+//Note that, apples from the same pack can be distributed into different boxes.
+
+class Redistribution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int sum =0;
+        for(int i : apple){
+            sum+=i;
+        }
+        Arrays.sort(capacity);
+        int ans =0;
+        int cap =0;
+        for(int i = capacity.length-1;i>=0;i--){
+            cap+=capacity[i];
+            ans++;
+            if(cap>=sum){
+                break;
+            }
+        }
+        return ans;
+    }
+}
