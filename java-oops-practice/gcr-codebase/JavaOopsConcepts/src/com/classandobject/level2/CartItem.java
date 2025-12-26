175b7a0f00c431689e81bcd76f406d3f48d1281e
Lucky Pal <pallucky936@gmail.com>
2025-12-26 17:53:18 +0530

    [Lucky Pal]: added Cart Item file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level2/CartItem.java
@@ -0,0 +1,36 @@
+package com.classandobject.level2;
+
+public class CartItem {
+	   private String itemName;
+	   private double price;
+	   private int quantity;
+	   // Constructor to initialize the attributes
+	   public CartItem(String itemName, double price, int quantity) {
+	       this.itemName = itemName;
+	       this.price = price;
+	       this.quantity = quantity;
+	   }
+	   // Method to add items to the cart
+	   public void addItem(int quantityToAdd) {
+	       this.quantity += quantityToAdd;
+	       System.out.println("Added " + quantityToAdd + " of " + this.itemName + " to the cart.");
+	   }
+	   // Method to remove items from the cart
+	   public void removeItem(int quantityToRemove) {
+	       if (quantityToRemove > this.quantity) {
+	           System.out.println("Cannot remove " + quantityToRemove + " of " + this.itemName + " as there are only " + this.quantity + " left.");
+	       } else {
+	           this.quantity -= quantityToRemove;
+	           System.out.println("Removed " + quantityToRemove + " of " + this.itemName + " from the cart.");
+	       }
+	   }
+	   // Method to calculate the total cost of the items in the cart
+	   public double totalCost() {
+	       return this.price * this.quantity;
+	   }
+	   // Method to display the item details
+	   public void displayItem() {
+	       System.out.println("Item: " + this.itemName + ", Price: $" + this.price + ", Quantity: " + this.quantity);
+	   }
+
+}
