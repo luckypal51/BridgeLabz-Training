d028bd9c42a6bbf9df865bbb54a119e5fabc9867
Lucky Pal <pallucky936@gmail.com>
2025-12-26 16:31:43 +0530

    [Lucky Pal]: added Mobile Phone Print file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level1/MobilePrint.java
@@ -0,0 +1,14 @@
+package com.classandobject.level1;
+
+public class MobilePrint {
+      public static void main(String[] args) {
+    	  MobilePhone mobile1=new MobilePhone("VIVO","VIVO V29" , 15999);
+    		MobilePhone mobile2=new MobilePhone("ONE PLUS","ONE PLUS nord4",39999);
+    		MobilePhone mobile3=new MobilePhone("APPLE","iphone pro16",79999);
+    		
+    		mobile1.diplayMobile();
+    		mobile2.diplayMobile();
+    		mobile3.diplayMobile();
+
+      }
+}
