277a0d5dae2b3302bf3568fb56325705d9f071d5
Lucky Pal <pallucky936@gmail.com>
2025-12-28 12:34:31 +0530

    [Lucky Pal]: added BankAccount file

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructinstance/level1/BankAccount.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructinstance/level1/BankAccount.java
@@ -1,5 +1,19 @@
 package com.constructinstance.level1;
 
 public class BankAccount {
-
+       public long accountNumber;
+       protected String accountHolder;
+       private long balance;
+       
+       public BankAccount(long accountNumber,String accountHolder,long balance) {
+    	   this.accountNumber = accountNumber;
+    	   this.accountHolder = accountHolder;
+    	   this.balance = balance;
+       }
+       public void setBalance(long balance) {
+    	   this.balance = balance;
+       }
+       public long getBalance() {
+    	   return this.balance;
+       }
 }
