d35e65ae46484b4f7dff5f925e0dfda075792926
Lucky Pal <pallucky936@gmail.com>
2025-12-29 14:13:03 +0530

    [Lucky Pal]: added Book file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/keywords/level1/Book.java
@@ -0,0 +1,46 @@
+package com.keywords.level1;
+
+public class Book {
+	 
+	   private static String libraryName;
+	 
+	   private String title;
+	   private String author;
+	   private final String isbn; 
+	   
+	   public Book(String title, String author, String isbn) {
+	       this.title = title;
+	       this.author = author;
+	       this.isbn = isbn;
+	   }
+	  
+	   public static void setLibraryName(String name) {
+	       libraryName = name;
+	   }
+	  
+	   public static void displayLibraryName() {
+	       System.out.println("Library Name: " + libraryName);
+	   }
+	   
+	   public void displayBookDetails() {
+	     
+	       if (this instanceof Book) {
+	           System.out.println("Title: " + title);
+	           System.out.println("Author: " + author);
+	           System.out.println("ISBN: " + isbn);
+	       } else {
+	           System.out.println("The object is not an instance of the Book class.");
+	       }
+	   }
+	 
+	   public String getTitle() {
+	       return title;
+	   }
+	   public String getAuthor() {
+	       return author;
+	   }
+	   public String getIsbn() {
+	       return isbn;
+	   }
+
+}
