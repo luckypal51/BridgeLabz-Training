68caa93407c9f7366bbdfbbb9e02af11f475a957
Lucky Pal <pallucky936@gmail.com>
2025-12-30 18:55:18 +0530

    [Lucky Pal]: added Product file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/Product.java
@@ -0,0 +1,20 @@
+package com.objectmodeling;
+class Product {
+    private int productId;
+    private String name;
+    private double price;
+
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
