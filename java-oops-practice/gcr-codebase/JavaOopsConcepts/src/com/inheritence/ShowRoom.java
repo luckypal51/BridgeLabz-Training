d1efe1c5f1d56974df3a8793ec4beb765a154e45
Lucky Pal <pallucky936@gmail.com>
2025-12-31 19:03:11 +0530

    [Lucky Pal]: added Vehicle inheritence file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/ShowRoom.java
@@ -0,0 +1,16 @@
+package com.inheritence;
+
+public class ShowRoom {
+	public static void main(String[] args) {
+		Vehicle v = new Vehicle(150,"Diesel");
+		Car car = new Car(210,"Diesel/Petrol",5);
+		Truck truck = new Truck(180,"Diesel",3);
+		MotorCycle mt = new MotorCycle(180,"Petrol",2);
+		
+		v.displayInfo();
+		car.displayInfo();
+		truck.displayInfo();
+		mt.displayInfo();
+		
+	}
+}
