96eba5060a839fd873810d8d096a686eec8dead1
Lucky Pal <pallucky936@gmail.com>
2025-12-31 19:15:56 +0530

    [Lucky Pal]: added Library inheritence file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritence/Library.java
@@ -0,0 +1,8 @@
+package com.inheritence;
+
+public class Library {
+    public static void main(String[] args) {
+    	Author author = new Author("Java programming","2025","Lucky Pal","Eat code repeat");
+    	author.displayInfo();
+    }
+}
