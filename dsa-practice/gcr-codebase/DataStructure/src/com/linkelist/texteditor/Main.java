e879a4255947148760eb6f0b2204b2012a32e8a4
Lucky Pal <pallucky936@gmail.com>
2026-01-02 19:36:29 +0530

    [Lucky Pal]: added texteditor file

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/linkelist/texteditor/Main.java
@@ -0,0 +1,29 @@
+package com.linkelist.texteditor;
+
+public class Main {
+	 public static void main(String[] args) {
+
+	        TextEditor editor = new TextEditor();
+
+	        editor.addState("Lucky");
+	        editor.addState("Lucky Pal");
+	        editor.addState("Lucky Pal is");
+	        editor.addState("Lucky Pal is best");
+
+	        editor.displayCurrentState();
+
+	        editor.undo();
+	        editor.displayCurrentState();
+
+	        editor.undo();
+	        editor.displayCurrentState();
+
+	        editor.redo();
+	        editor.displayCurrentState();
+
+	        editor.addState("Lucky coder");
+	        editor.displayCurrentState();
+
+	        editor.redo();  // redo cleared after new state
+	    }
+}
---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/linkelist/texteditor/TextState.java
@@ -0,0 +1,13 @@
+package com.linkelist.texteditor;
+
+class TextState {
+    String content;
+    TextState prev;
+    TextState next;
+
+    TextState(String content) {
+        this.content = content;
+        this.prev = null;
+        this.next = null;
+    }
+}
