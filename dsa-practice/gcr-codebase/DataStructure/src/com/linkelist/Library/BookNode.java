919419ff5ffbfe5da830b2e74910ddd636b06b0a
Lucky Pal <pallucky936@gmail.com>
2026-01-02 19:34:45 +0530

    [Lucky Pal]: added LinkList-Library file

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/linkelist/Library/BookNode.java
@@ -0,0 +1,22 @@
+package com.linkelist.Library;
+
+class BookNode {
+    int bookId;
+    String title;
+    String author;
+    String genre;
+    boolean isAvailable;
+
+    BookNode next;
+    BookNode prev;
+
+    BookNode(int bookId, String title, String author, String genre, boolean isAvailable) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.isAvailable = isAvailable;
+        this.next = null;
+        this.prev = null;
+    }
+}
