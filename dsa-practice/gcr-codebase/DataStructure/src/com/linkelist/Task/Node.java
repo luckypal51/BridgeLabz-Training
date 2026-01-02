1097e4260a0f677579c5efed069d4ea333e7718c
Lucky Pal <pallucky936@gmail.com>
2026-01-02 19:35:49 +0530

    [Lucky Pal]: added Task file

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/linkelist/Task/Node.java
@@ -0,0 +1,19 @@
+package com.linkelist.Task;
+
+import java.time.LocalDate;
+
+public class Node {
+     long id;
+     String name;
+     int priority;
+     LocalDate due;
+     Node next;
+	 public Node(long id, String name, int priority, LocalDate due) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.priority = priority;
+		this.due = due;
+	 }
+     
+}
