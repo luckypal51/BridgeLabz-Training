719d50cb4e5710aae070f708972c72ec300113bc
Lucky Pal <pallucky936@gmail.com>
2026-01-05 12:38:03 +0530

    [Lucky Pal]: added HashMap Implementation Sequence  file

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/stackqueue/hashmap/Node.java
@@ -0,0 +1,14 @@
+package com.stackqueue.hashmap;
+
+
+public class Node {
+    int key;
+    int val;
+    Node next;
+
+    Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+    }
+}
