4d6b9a4ae5d76bb60db03a13e2e7d72fd22a4555
Lucky Pal <pallucky936@gmail.com>
2026-01-06 15:51:47 +0530

    [Lucky Pal]: added Insertion Sort file

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/sortingalogorithm/insertion/Insertion.java
@@ -0,0 +1,26 @@
+package com.sortingalogorithm.insertion;
+
+public class Insertion {
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 5, 2};
+
+        insertionSort(arr);
+
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+    }
+	public static void insertionSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];    
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key; 
+        }
+    }
+
+}
