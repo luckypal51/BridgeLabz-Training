2cf6c4f92364a50e7291403b5145b7ad45befa8c
Lucky Pal <pallucky936@gmail.com>
2026-01-12 12:15:30 +0530

    [Lucky Pal]: added ReadFile file

---
+++ dsa-practice/gcr-codebase/DataStructure/src/com/timecomplexicity/ReadFile.java
@@ -0,0 +1,28 @@
+package com.timecomplexicity;
+import java.io.*;
+
+
+public class ReadFile {
+
+	public static void main(String[] args){
+		try {
+			FileReader file = new FileReader("File/Name.txt");
+			
+			
+			BufferedReader reader = new BufferedReader(file);
+			
+			System.out.println("We are going to read file 'Name.txt' line by line\n");
+			String line;
+			
+			while((line=reader.readLine())!=null) {
+				System.out.println(line);
+			}
+			
+			reader.close();
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
