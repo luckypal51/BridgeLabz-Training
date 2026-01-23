fec73d1bc9069348b566c75e31a0f78e58e2f2dd
Lucky Pal <pallucky936@gmail.com>
2026-01-23 16:55:28 +0530

    [Lucky Pal]: added ProductInventory file

--- dsa-practice/scenario-code-base/ScenarioQuestion/src/Tree/ProductInventory/Main.java
+++ dsa-practice/scenario-code-base/ScenarioQuestion/src/Tree/ProductInventory/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		ven.insert(new Product(65101,"iPhone",1000000));
 		ven.insert(new Product(65102,"Dal",150));
 		ven.insert(new Product(54103,"Phone",1000000));
-		ven.insert(new Product(54104,"xxxxx",150));
+		ven.insert(new Product(54104,"sdfghjk",150));
 		ven.insert(new Product(45105,"qwisis",45000));
 		ven.insert(new Product(21026,"ewids",45000));
 		ven.insert(new Product(10445,"dfi",1000000));
