22a9f5f77ee4db5df504340782d496fdc0caf5d6
Lucky Pal <pallucky936@gmail.com>
2026-01-28 17:46:14 +0530

    [Lucky Pal]: added exception files

---
+++ java-collections-practice/gcr-codebase/Question/src/exceptions/banksystem/InsufficientBalanceException.java
@@ -0,0 +1,10 @@
+package exceptions.banksystem;
+
+class InsufficientBalanceException extends Exception {
+ 
+	private static final long serialVersionUID = 1L;
+
+	public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
---
+++ java-collections-practice/gcr-codebase/Question/src/exceptions/banksystem/Main.java
@@ -0,0 +1,17 @@
+package exceptions.banksystem;
+
+public class Main {
+	 public static void main(String[] args) {
+	        BankAccount account = new BankAccount(5000);
+
+	        try {
+	            account.withdraw(6000);   
+	        }
+	        catch (InsufficientBalanceException e) {
+	            System.out.println(e.getMessage());
+	        }
+	        catch (IllegalArgumentException e) {
+	            System.out.println(e.getMessage());
+	        }
+	    }
+}
