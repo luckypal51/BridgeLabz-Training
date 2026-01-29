ac4539a6dff5a294ad45e697e06aa4950725c012
Lucky Pal <pallucky936@gmail.com>
2026-01-29 13:29:33 +0530

    [Lucky Pal]: added surpasswarning file

---
+++ java-collections-practice/gcr-codebase/Question/src/anotation/surpresswarning/Main.java
@@ -0,0 +1,10 @@
+package anotation.surpresswarning;
+
+import java.util.ArrayList;
+
+public class Main {
+	@SuppressWarnings("unchecked")
+   public static void main(String[] args) {
+	   ArrayList a = new ArrayList<>();
+   }
+}
