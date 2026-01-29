ff3025266cc894ce30ed05a5a5807b49083cdcb1
Lucky Pal <pallucky936@gmail.com>
2026-01-29 09:46:23 +0530

    [Lucky Pal]: added Date extraction

---
+++ java-collections-practice/gcr-codebase/Question/src/regex/extractdates/ExtractDates.java
@@ -0,0 +1,21 @@
+package regex.extractdates;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractDates {
+    public static void main(String[] args) {
+
+        String text = "The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.";
+
+        String regex = "\\b(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        System.out.println("Extracted Dates:");
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
