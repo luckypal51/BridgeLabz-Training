ea5e063b3b39ac0cb18accd0c49101cf75c54093
Lucky Pal <pallucky936@gmail.com>
2026-01-29 20:37:36 +0530

    [Lucky Pal]: added json field main file

--- java-collections-practice/gcr-codebase/Question/src/anotation/jsonfield/Main.java
+++ java-collections-practice/gcr-codebase/Question/src/anotation/jsonfield/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         User user = new User("Lucky", 21, "luckypal051203");
-
         String json = JsonSerializer.toJson(user);
 
         System.out.println(json);
